37ec12094b1e94e92f198f1ddf160df8004d05d4
Maria Silaghi <smaria.oana@yahoo.com>
2025-07-29 18:43:04 +0300

    Add login/logout pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
-import "@/styles/globals.css";
+import '../styles/globals.css'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import { getToken } from '../lib/userAuth'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+const MyApp = ({ Component, pageProps }) => {
+  const router = useRouter()
+
+  useEffect(() => {
+    const isLoginPage = router.pathname === '/login'
+    const isPublicPage = ['/login', '/register'].includes(router.pathname)
+
+    if (!getToken() && !isPublicPage) {
+      router.push('/login')
+    }
+  }, [router])
+
+  return <Component {...pageProps} />
 }
+
+export default MyApp
